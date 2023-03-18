d26d01ac8542133cdd14839035392cabf77743ec
EC2 Default User <ec2-user@ip-172-31-31-98.ec2.internal>
2023-03-18 18:37:10 +0000

    should define your key file, vpc_id and subnet_id

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "instance_public_ip" {
   value = aws_instance.bastion.public_ip
+  description = "The public IP address of the bastion instance."
 }
 
 output "instance_private_ip" {
   value = aws_instance.bastion.private_ip
+  description = "The private IP address of the bastion instance."
 }
